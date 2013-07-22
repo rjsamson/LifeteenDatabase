449b40ef4a3b7a3f7ab106bc67f31947f504e7a9
Aaron Pfoltzer <aaron.pfoltzer@gmail.com>
2013-07-21 23:17:34 -0400

    image not caching in dropdon, etc

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,12 @@
 class Contact < ActiveRecord::Base
-	validates :first_name, :last_name, :address1, presence: true
+	validates :first_name, :last_name, presence: true
 	validates_format_of :phone, :cell, :fax, :work_phone, 
     	message: "must be a valid telephone number.",
     	with: /^[\(\)0-9\- \+\.]{10,20} *[extension\.]{0,9} *[0-9]{0,5}$/i,
     	multiline: true,
     	allow_blank: true
 	validates :graduating_class, allow_blank: true, format: {with: /(19|20)\d{2}/i }
-	 
+
 #	belongs_to :address1, foreign_key: "address1_id", class_name: "Address"
 #	belongs_to :address2, foreign_key: "address2_id", class_name: "Address"
 #	belongs_to :college_address, foreign_key: "college_address_id", class_name: "Address"
